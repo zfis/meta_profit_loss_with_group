7ecdbe87505a9bb21b8709c993860803d3732c1f
ZeroSystems <sales@erpzero.com>
2019-10-18 11:06:40 +0200

    Update profit_loss_report_wizard.py

--- meta_profit_loss_with_group/models/profit_loss_report_wizard.py
+++ meta_profit_loss_with_group/models/profit_loss_report_wizard.py
@@ -1,7 +1,8 @@
 from odoo import models, fields, api, _
-import odoo.addons.decimal_precision as dp
 from datetime import datetime, timedelta
-from odoo.exceptions import UserError
+from odoo.service import db
+from odoo.models import check_method_name
+from odoo.exceptions import AccessError, UserError
 
 
 class ProfitLossGroupReportWizard(models.Model):
